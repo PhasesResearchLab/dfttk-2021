24b6f014792778344b2dc781e7389432d2400331
Yi Wang <yiwang62@gmail.com>
2021-04-05 09:31:42 -0400

    Update run_dfttk.py

--- dfttk/scripts/run_dfttk.py
+++ dfttk/scripts/run_dfttk.py
@@ -15,7 +15,7 @@ import copy
 import os
 import sys
 import shutil
-import dfttk.scripts.user_SETTINGS
+import dfttk.scripts.user_SETTINGS as user_SETTINGS
 
 
 def get_abspath(path):
@@ -198,7 +198,6 @@ def get_wf_single(structure, WORKFLOW="get_wf_gibbs", settings={}):
     sym_reduce = settings.get('sym_reduce', False)
     order = settings.get('order', 2)
     conventional = settings.get('conventional', False)
-    store_raw_vasprunxml = settings.get('store_raw_vasprunxml', False)
 
     """
     stencils = settings.get('stencils', [0.01])
@@ -257,8 +256,7 @@ def get_wf_single(structure, WORKFLOW="get_wf_gibbs", settings={}):
                  override_default_vasp_params=override_default_vasp_params, modify_incar_params=modify_incar_params,
                  modify_kpoints_params=modify_kpoints_params, verbose=verbose, phonon_supercell_matrix_min=phonon_supercell_matrix_min,
                  phonon_supercell_matrix_max=phonon_supercell_matrix_max, optimize_sc=optimize_sc, level=level,
-                 force_phonon=force_phonon, stable_tor=stable_tor, store_volumetric_data=store_volumetric_data,
-                 store_raw_vasprunxml=store_raw_vasprunxml)
+                 force_phonon=force_phonon, stable_tor=stable_tor, store_volumetric_data=store_volumetric_data)
     elif WORKFLOW == "born":
         wf = get_wf_borncharge(structure=structure, metadata=metadata, db_file=">>db_file<<", isif=2, name="born charge",
                       vasp_cmd=">>vasp_cmd<<", override_default_vasp_params=override_default_vasp_params,
@@ -306,8 +304,8 @@ def run(args):
     TAG = args.TAG                      # Metadata from the command line
     APPEND = args.APPEND                # Append calculations, e.g. appending volumes or phonon or born
 
-    if os.path.exists('SETTING.yaml'): #treat 'SETTING.yaml' as globally accessible
-        user_SETTINGS.user_settings=loadfn('SETTING.yaml')
+    if os.path.exists('SETTINGS.yaml'): #treat settings in 'SETTINGS.yaml' as globally accessible
+        user_SETTINGS.user_settings=loadfn('SETTINGS.yaml')
 
     if os.path.exists('db.json'):
         db_file = 'db.json'
