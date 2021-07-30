47740bbcca90fca7ec7bca3f5579b6c8d8deebe6
Yi Wang <yiwang62@gmail.com>
2021-07-30 13:07:33 -0400

    Update ftasks.py

--- dfttk/ftasks.py
+++ dfttk/ftasks.py
@@ -955,7 +955,7 @@ class CheckRelaxation(FiretaskBase):
         common_kwargs = self.get('common_kwargs')
         #override_default_vasp_params = common_kwargs.get('override_default_vasp_params',{})
         #powerups_options = get_powerups_options(override_default_vasp_params)
-        modify_incar_params = common_kwargs.get('override_default_vasp_params',{})
+        modify_incar_params = common_kwargs.get('modify_incar_params',{})
         powerups_options = get_powerups_options(modify_incar_params)
         return Customizing_Workflows(detour_fws, powerups_options=powerups_options)
 
