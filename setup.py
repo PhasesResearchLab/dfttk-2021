9f282256ce14f1ffe77c5c8e194e8d7e7b8afbbe
Adam M. Krajewski <54290107+amkrajewski@users.noreply.github.com>
2023-11-17 13:30:29 -0500

    - remove Python 3.6 and 3.7 from compatibility classifiers for PyPI

--- setup.py
+++ setup.py
@@ -46,8 +46,6 @@ setup(
         'License :: OSI Approved :: MIT License',
 
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8'
     ],
     entry_points={
